c34f1751aee40c43e3a76ffb1b11ec797f3824d9
Sanny Do <sanny.do94@gmail.com>
2020-08-18 06:03:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
-def loop_message_five_times = greetings
-  puts "Hello"
+def loop_message_five_times(arrays)
+counter = 0
+pets = [ "Dogs", "Cats","Birds", "Hamsters", "Lizards" ]
+
+while pets[counter] do
+  puts pets[counter]
+  counter += 1
 end
-greetings
-greetings
-greetings
-greetings
-greetings
