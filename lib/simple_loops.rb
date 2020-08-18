2477c255fb1d873de8085502de31758719163455
Sanny Do <sanny.do94@gmail.com>
2020-08-18 06:14:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,12 +8,20 @@ count = 0
 end
 
 
-def loop_message_n_times
-  
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
 end
 
-def output_array
-  
+def output_array(array)
+  count = 0 
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
 end
 
 def return_string_array
